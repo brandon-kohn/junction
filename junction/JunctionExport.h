b80706d174317946585d2dd4a51f19678062c752
Brandon Kohn <blkohn@hotmail.com>
2018-03-08 09:33:11 -0500

    Changes to export defines.

--- junction/JunctionExport.h
+++ junction/JunctionExport.h
@@ -1,18 +1,19 @@
 #pragma once
-#if defined(TURF_USE_DLL) || defined(TURF_EXPORTS_API)
+
+#ifdef JUNCTION_STATIC_LIB
+    #define JUNCTION_API 
+#else
     #ifdef _WIN32
-        #ifdef TURF_EXPORTS_API
-            #define TURF_API __declspec(dllexport)
+        #ifdef JUNCTION_EXPORTS_API
+            #define JUNCTION_API __declspec(dllexport)
         #else
-            #define TURF_API __declspec(dllimport)
+            #define JUNCTION_API __declspec(dllimport)
         #endif
     #else
-        #ifdef TURF_EXPORTS_API
-            #define TURF_API __attribute__ ((visibility ("default")))
+        #ifdef JUNCTION_EXPORTS_API
+            #define JUNCTION_API __attribute__ ((visibility ("default")))
         #else
-            #define TURF_API
+            #define JUNCTION_API
+        #endif
     #endif
 #endif
-    #define TURF_API 
-#endif
-
